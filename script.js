8337cc2b88f57dc978605b1265eac3cd688225a6
CenaBabajannjhad <cenababajannjhad@gmail.com>
2024-07-14 17:15:38 +0330

    bugs

--- script.js
+++ script.js
@@ -1,25 +1,111 @@
-let input1 = '1';
-let input2 = '2';
+let a = '34';
+let b = '13';
 
 function solution(a, b){
-
-    let output ;
-    let shorter , longest;
-    let argA = Number(a);
-    let argB = Number(b);
+    let paramOne = Number(a);
+    let paramTwo = Number(b);
     
-    if(argA > argB){
-        longest = argA.toString();
-        shorter = argB.toString();
+    if(!paramOne || !paramTwo){
+        let strOutput = forString()
+        return strOutput
 
-    }else if(argB > argA){
-        longest = argB.toString();
-        shorter = argA.toString();
-
+    }else if(paramOne || paramOne){
+        let numberOutput = forNumbers()
+        return numberOutput
     }
     
-    output = shorter + longest + shorter
-    return output
 }
-  
-console.log(solution(input1 , input2));
+
+function forNumbers(a , b){
+
+    if(a > b){
+       let paramOne = a.toString() 
+       let paramTwo = b.toString() 
+       
+       return paramTwo + paramOne + paramTwo
+
+    }else if(b > a){
+        return paramOne + paramTwo + paramOne        
+    }
+}
+
+
+function forString(a , b){
+
+    let lenParamOne = a.length;
+    let lenParamTwo = b.length;
+
+    if(lenParamOne > lenParamTwo){
+        return b + a + b
+    }else {
+        return a + b + a
+    }
+}
+
+
+
+console.log(solution(a , b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let output ;
+// let shorter , longest;
+// let argA = Number(a);
+// let argB = Number(b);
+
+// if(argA === NaN){
+//     console.log('error')
+
+// }else if(argA > argB){
+//     longest = argA.toString();
+//     shorter = argB.toString();
+
+// }else if(argB > argA){
+//     longest = argB.toString();
+//     shorter = argA.toString();
+// }
+
+// output = shorter + longest + shorter
+// return output
