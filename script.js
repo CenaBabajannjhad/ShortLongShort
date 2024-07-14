69768e8a433c1a93ea042253b675d43193ff7c52
CenaBabajannjhad <cenababajannjhad@gmail.com>
2024-07-14 16:36:45 +0330

    finished for numbers

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 let input1 = '1';
-let input2 = '22';
+let input2 = '2';
 
 function solution(a, b){
+
     let output ;
     let shorter , longest;
     let argA = Number(a);
     let argB = Number(b);
     
     if(argA > argB){
-        longest = argA;
-        shorter = argB;
+        longest = argA.toString();
+        shorter = argB.toString();
 
     }else if(argB > argA){
-        longest = argB;
-        shorter = argA;
+        longest = argB.toString();
+        shorter = argA.toString();
+
     }
     
     output = shorter + longest + shorter
